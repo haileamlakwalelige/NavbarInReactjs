b264b3173db673ed174e6b4c8754a513db7d79ce
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-02-11 13:28:24 +0300

    Navbar In React js

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <header>
+        <h3>Logo</h3>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">My work</a>
+            <a href="#">Blog</a>
+            <a href="#">About Me</a>
+            
+        </nav>
+    </header>
+  );
+}
+
+export default Navbar
